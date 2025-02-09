25a1c1f2c613f6a47959b9b3198f3f9d26d7f55e
unixpup <dev@fault.wtf>
2025-02-08 22:17:42 -0300

    Enable more Astro options

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,32 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
+import partytown from "@astrojs/partytown";
+
+import react from "@astrojs/react";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://unixpup.github.io",
+  trailingSlash: "always",
+  output: "static",
+  integrations: [partytown(), react()],
+  prefetch: {
+    prefetchAll: true,
+    defaultStrategy: "load",
+  },
+  i18n: {
+    defaultLocale: "en",
+    locales: ["en", "es"],
+  },
+  image: {
+    experimentalLayout: "responsive",
+  },
+  experimental: {
+    responsiveImages: true,
+    svg: true,
+    clientPrerender: true,
+    contentIntellisense: true,
+  },
 });
+
