5265f8bf498ba6e36ad7c0bc4a7f2819f6c89036
unixpup <dev@fault.wtf>
2025-03-30 02:05:08 -0300

    tuff

--- next.config.ts
+++ next.config.ts
@@ -1,24 +1,6 @@
 import { NextConfig } from 'next';
 
-const isGithubActions: boolean = process.env.GITHUB_ACTIONS === 'true' || false;
-
-let assetPrefix: string = '';
-let basePath: string = '/';
-
-if (isGithubActions) {
-  // trim off `<owner>/`
-  const repo: string = process.env.GITHUB_REPOSITORY 
-    ? process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
-    : '';
-
-  assetPrefix = `/${repo}/`;
-  basePath = `/${repo}`;
-}
-
 const config: NextConfig = {
-  assetPrefix: assetPrefix,
-  basePath: basePath,
-  output: 'export',
 };
 
 export default config;
