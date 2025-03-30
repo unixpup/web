cb1c23bfd3020e2145c290f920477581819dc9ec
unixpup <dev@fault.wtf>
2025-03-30 02:29:10 -0300

    Move to Canary

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,14 @@
 import { NextConfig } from 'next';
 
+const withMDX = require('@next/mdx')()
+
 const config: NextConfig = {
-};
+	pageExtensions: ['ts', 'tsx', 'mdx'],
+	experimental: {
+		cssChunking: true,
+		dynamicIO: true,
+		mdxRs: true,
+	},
+} satisfies NextConfig
 
 export default config;
