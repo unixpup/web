2d43e8e31c5898e5cc27649ab4e2db056fdb3349
unixpup <dev@fault.wtf>
2025-03-30 02:54:39 -0300

    Replace threads with dithering

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,33 @@
 import { Suspense } from "react"
+import Dither from '../Dither/Dither.tsx';
 // import { StaticComponent, DynamicComponent, Fallback } from "@/app/ui"
 
 export const experimental_ppr = true
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+	<div style={{ width: '100%', height: '600px', position: 'relative' }}>
 
-    </div>
+  <Dither
+
+    waveColor={[0.5, 0.5, 0.5]}
+
+    disableAnimation={false}
+
+    enableMouseInteraction={true}
+
+    mouseRadius={0.3}
+
+    colorNum={4}
+
+    waveAmplitude={0.3}
+
+    waveFrequency={3}
+
+    waveSpeed={0.05}
+
+  />
+
+</div>
   );
 }
