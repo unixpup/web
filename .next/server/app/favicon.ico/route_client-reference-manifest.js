bbbe10311415e6c3e5b6e4d55a9e4f70d38961f2
unixpup <dev@fault.wtf>
2025-03-30 01:36:03 -0300

    big oops

--- .next/server/app/favicon.ico/route_client-reference-manifest.js
+++
@@ -1,2 +0,0 @@
-globalThis.__RSC_MANIFEST = globalThis.__RSC_MANIFEST || {};
-globalThis.__RSC_MANIFEST["/favicon.ico/route"] = {"moduleLoading":{"prefix":"/change-me-to-your-repo/_next/","crossOrigin":null},"clientModules":{},"ssrModuleMapping":{},"edgeSSRModuleMapping":{},"rscModuleMapping":{},"edgeRscModuleMapping":{},"entryCSSFiles":{},"entryJSFiles":{}}
--- .next/static/development/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {"__rewrites":{"afterFiles":[],"beforeFiles":[{"source":"/change-me-to-your-repo//_next/:path+","destination":"/change-me-to-your-repo/_next/:path+"}],"fallback":[]},"/_app":["static/chunks/pages/_app.js"],"/_error":["static/chunks/pages/_error.js"],"sortedPages":["/_app","/_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
